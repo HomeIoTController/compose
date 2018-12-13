774a2b2877377368381b97f7280cadf61802016d
Daniel Marchena Parreira <daniel@Daniels-MacBook-Pro.local>
2018-12-12 20:58:38 -0500

    Full support for Kafka v1

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Home Controller Setup
+
+echo ">>> Setup started!"
+
+echo ">>> Cloning repositories"
+for app in graphql-api eeg-brainwave-client audio-client web-client eeg-brainwave-api; do
+		git clone "https://github.com/HomeIoTController/$app.git" "../$app"
+done
+
+echo ">>> Destroying existent containers"
+docker-compose down --rmi all
+rm -rf ./database-*
+rm -rf ./classifiers
+
+echo ">>> Creating application containers"
+docker-compose up -d --build
+
+cho ">>> Setup complete!"
